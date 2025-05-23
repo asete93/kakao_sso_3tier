09a809a4b7b04a90761b622bdad573e4ef67afb8
root <root@martin.tail7cc40d.ts.net>
2025-05-23 09:01:14 +0000

    add jwt logic

---
+++ Backend/src/main/java/com/camel/api/services/token/service/JwtTokenService.java
@@ -0,0 +1,134 @@
+package com.camel.api.services.token.service;
+
+import java.nio.charset.StandardCharsets;
+import java.sql.Date;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.camel.common.CustomMap;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+@Service
+public class JwtTokenService {
+    
+    @Value("{jwt.token.secret}")
+    private String JWT_TOKEN_SECRET;
+
+    private String JWT_ISSUER = "CAMEL_API";
+
+    private Integer ONE_SEC = 1000;
+    private Integer ONE_MINUTE = ONE_SEC * 60;
+    private Integer ONE_HOUR = ONE_MINUTE * 60;
+
+    // access_token 유효시간 (30분)
+    private Integer ACCESS_TOKEN_TIME_MILLIS = ONE_MINUTE * 30 ;
+
+    // refresh_token 유효시간 (1 day)
+    private Integer REFRESH_TOKEN_TIME_MILLIS = ONE_HOUR * 24;
+
+
+    
+
+
+
+    /* ****************************************************************************************
+     * Title            :   JWT Token Map 생성
+     * Scope            :   public
+     * Function Name    :   createJwtTokenMap                                                    
+     * ----------------------------------------------------------------------------------------
+     * Description      :   access_token, refresh_token 이 담긴 CustomMap 생성
+     * 
+     ******************************************************************************************/
+    public CustomMap createJwtTokenMap(CustomMap claim) throws Exception {
+        CustomMap tokenMap = new CustomMap();
+
+
+        return tokenMap;
+    }
+
+
+
+
+    /* ****************************************************************************************
+     * Title            :   Access_token 생성 (JWT)
+     * Scope            :   private
+     * Function Name    :   createAccessToken                                                    
+     * ----------------------------------------------------------------------------------------
+     * Description      :   access_token 생성
+     * 
+     ******************************************************************************************/
+    private String createAccessToken(CustomMap claim) throws Exception {
+        SecretKey key = Keys.hmacShaKeyFor(JWT_TOKEN_SECRET.getBytes(StandardCharsets.UTF_8));
+
+        long nowMillis = System.currentTimeMillis();
+        Date now = new Date(nowMillis);
+        Date exp = new Date(nowMillis + ACCESS_TOKEN_TIME_MILLIS);
+
+        return Jwts.builder()
+                .setSubject(claim.getString("userId"))
+                .setIssuer(JWT_ISSUER)
+                .setIssuedAt(now)
+                .setExpiration(exp)
+                .setClaims(claim)
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+
+
+
+    /* ****************************************************************************************
+     * Title            :   Refresh_token 생성 (JWT)
+     * Scope            :   private
+     * Function Name    :   createRefreshToken                                                    
+     * ----------------------------------------------------------------------------------------
+     * Description      :   refresh_token 생성
+     * 
+     ******************************************************************************************/
+    private String createRefreshToken() throws Exception {
+
+        return "";
+    }
+
+
+
+
+
+    /* ****************************************************************************************
+     * Title            :   토큰 검증
+     * Scope            :   public
+     * Function Name    :   verifyToken                                                    
+     * ----------------------------------------------------------------------------------------
+     * Description      :   토큰 유효성 검증
+     * 
+     ******************************************************************************************/
+    public Boolean verifyToken(String token) throws Exception {
+
+        return true;
+    }
+
+
+
+
+
+
+
+    /* ****************************************************************************************
+     * Title            :   토큰 Parser
+     * Scope            :   public
+     * Function Name    :   tokenParser                                                    
+     * ----------------------------------------------------------------------------------------
+     * Description      :   토큰 파서
+     * 
+     ******************************************************************************************/
+    public CustomMap tokenParser(String token) throws Exception {
+
+        return new CustomMap();
+    }
+}
